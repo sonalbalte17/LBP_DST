a165725d6f1105f914b6c1f14942a8bae69e39cc
sonalbalte17 <sonalbalte17@gmail.com>
2022-05-12 11:31:48 -0400

    commit 05/12/2022

---
+++ src/HackerRank/lbp_009.java
@@ -0,0 +1,19 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class lbp_009 {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        int d,sum=0;
+        while(n!=0)
+        {
+            d=n%10;
+            if(d%2==0)
+                sum=sum+d;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
